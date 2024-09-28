772dddfabef0d02c9aadf1b5d085bd9c70e00a75
Marco Kleimans <kleimansx@isi.edu>
2024-09-28 17:46:01 -0500

    CSS + JS push

---
+++ assets/scripts.js
@@ -0,0 +1,20 @@
+//Background rotation
+$(document).ready(function() {
+    var $body = $('body');
+    var classes = ['', 'img_2', 'img_3']; // The classes to cycle through, including no class (empty string).
+    var index = 0;
+
+    setInterval(function() {
+        // Remove any previous class
+        $body.removeClass(classes.join(' '));
+
+        // Add the next class in the cycle
+        $body.addClass(classes[index]);
+
+        // Update index for next cycle
+        index = (index + 1) % classes.length; // Cycle through the classes
+    }, 1500); // 10,000 milliseconds = 10 seconds
+});
+
+
+
