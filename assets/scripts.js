2249f3c2d72663528041bc176a81811f8b4f6a1b
Marco Kleimans <kleimansx@isi.edu>
2024-09-29 12:30:42 -0500

    feature: Hamburger Menu for Mobile Devices

--- assets/scripts.js
+++ assets/scripts.js
@@ -31,25 +31,36 @@ $(document).ready(function() {
         // Add 'active' class to the clicked menu item
         $(this).addClass('active');
     });
-});
+/***************
+ HAMBURGER MENU
+******************/
+    // Toggle menu open/close on clicking the bars icon
+    $('.hamburger-toggle').on('click', function() {
+        $('.hamburger-menu-container').toggleClass('open'); // Toggle the 'open' class
+    });
 
+    // Close the menu when a menu item is clicked
+    $('.hamburger-menu a').on('click', function() {
+        $('.hamburger-menu-container').removeClass('open'); // Remove 'open' class to close menu
+    });
+});
 //Background rotation
-$(document).ready(function() {
-    var $body = $('body');
-    var classes = ['', 'img_2', 'img_3']; // The classes to cycle through, including no class (empty string).
-    var index = 0;
-
-    setInterval(function() {
-        // Remove any previous class
-        $body.removeClass(classes.join(' '));
-
-        // Add the next class in the cycle
-        $body.addClass(classes[index]);
-
-        // Update index for next cycle
-        index = (index + 1) % classes.length; // Cycle through the classes
-    }, 1500); // 10,000 milliseconds = 10 seconds
-});
-
+// $(document).ready(function() {
+//     var $body = $('body');
+//     var classes = ['', 'img_2', 'img_3']; // The classes to cycle through, including no class (empty string).
+//     var index = 0;
+//
+//     setInterval(function() {
+//         // Remove any previous class
+//         $body.removeClass(classes.join(' '));
+//
+//         // Add the next class in the cycle
+//         $body.addClass(classes[index]);
+//
+//         // Update index for next cycle
+//         index = (index + 1) % classes.length; // Cycle through the classes
+//     }, 1500); // 10,000 milliseconds = 10 seconds
+// });
+//
 
 
