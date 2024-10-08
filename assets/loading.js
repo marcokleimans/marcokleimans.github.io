25f6f28628ae2f42f96665ede47fe85821f21fa1
Marco Kleimans <kleimansx@isi.edu>
2024-10-08 15:12:10 -0500

    new: 404 page

--- assets/loading.js
+++ assets/loading.js
@@ -1,21 +1,22 @@
 // Background rotation
 $(document).ready(function() {
-    var images = $('img'); // Select all the images
-    var currentIndex = 0;   // Start with the first image
-    var imageCount = images.length; // Get the number of images
 
-    // Function to cycle through the images
-    function rotateImages() {
-        // Hide the current image
-        images.eq(currentIndex).hide();
+    var $body = $('body');
+    var classes = ['', 'b2', 'b3'];
+    var index = 0;
 
-        // Update the index to the next image (cyclically)
-        currentIndex = (currentIndex + 1) % imageCount;
+    var preloadedImages = [];
+    preloadedImages[0] = new Image();
+    preloadedImages[0].src = '/assets/images/led_2.jpg';
+    preloadedImages[1] = new Image();
+    preloadedImages[1].src = '/assets/images/led_3.jpg';
 
-        // Show the next image
-        images.eq(currentIndex).show(); // Optional fade effect
-    }
+    setInterval(function() {
 
-    // Set the interval for image rotation (1.5 seconds)
-    setInterval(rotateImages, 1500); // 1500ms = 1.5 seconds
+        $body.removeClass(classes.join(' '));
+
+        $body.addClass(classes[index]);
+
+        index = (index + 1) % classes.length;
+    }, 1500);
 });
