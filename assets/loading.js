8fa1007161e879513160f98229b1d99eadf5daa9
marcokleimans <marcokleimans1998@gmail.com>
2026-07-20 13:54:58 -0500

    Tenaris updates
    - Added images
    - Added projects
    - Added description of job

--- assets/loading.js
+++ assets/loading.js
@@ -1,22 +1,22 @@
-// Background rotation
-$(document).ready(function() {
+// // Background rotation
+// $(document).ready(function() {
 
-    var $body = $('body');
-    var classes = ['', 'b2', 'b3'];
-    var index = 0;
+//     var $body = $('body');
+//     var classes = ['', 'b2', 'b3'];
+//     var index = 0;
 
-    var preloadedImages = [];
-    preloadedImages[0] = new Image();
-    preloadedImages[0].src = '/assets/images/led_2.jpg';
-    preloadedImages[1] = new Image();
-    preloadedImages[1].src = '/assets/images/led_3.jpg';
+//     var preloadedImages = [];
+//     preloadedImages[0] = new Image();
+//     preloadedImages[0].src = '/assets/images/led_2.jpg';
+//     preloadedImages[1] = new Image();
+//     preloadedImages[1].src = '/assets/images/led_3.jpg';
 
-    setInterval(function() {
+//     setInterval(function() {
 
-        $body.removeClass(classes.join(' '));
+//         $body.removeClass(classes.join(' '));
 
-        $body.addClass(classes[index]);
+//         $body.addClass(classes[index]);
 
-        index = (index + 1) % classes.length;
-    }, 1500);
-});
+//         index = (index + 1) % classes.length;
+//     }, 1500);
+// });
